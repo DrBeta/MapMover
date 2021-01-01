e52d9df6060bcdd18afbc23437f425a911bf2fdf
Heinz Doofenshmirtz <45474221+DrBeta@users.noreply.github.com>
2021-01-01 11:47:16 -0800

    made it generic to all people

--- mcmover.py
+++ mcmover.py
@@ -7,7 +7,7 @@ explainer = Text(app, text="Click the button and select the map .zip to install 
 def select_file():
     target = app.select_file(filetypes=[["Zip Files", "*.zip"]])
     handle = zipfile.ZipFile(target)
-    handle.extractall(r'c:\Users\nomen\AppData\Roaming\.minecraft\saves')
+    handle.extractall(r'c:\\%UserProfile%\AppData\Roaming\.minecraft\saves')
     app.info("Done", "Your map was successfully moved!")
 
 select_button = PushButton(app, command=select_file, text="Select Zip")
